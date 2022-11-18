e740dccd66e569ce9c697ac0b0ccd55709472ba6
EricH3344 <56049098+EricH3344@users.noreply.github.com>
2022-11-18 01:06:08 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 function handlePurchase() {
     window.alert("Coming soon.");
 }
+
+function purchasePage() {
+    const checkedBtn = document.querySelector('input[type="radio"][value="cd"]:checked');
+    if (checkedBtn) {
+        window.open("https://www.paypal.com/signin", '_blank', "width=700,height=600");    
+    }else {
+        window.open("./payment.html", "_blank", "width=700,height=600")
+    }
+}
